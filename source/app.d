6a12a592820fe4fe757e440edbce4f9ceef53d31
tanitta <ttata.trit@gmail.com>
2017-04-09 17:38:43 +0900

    Implement insert and remove.

--- source/app.d
+++ source/app.d
@@ -81,6 +81,7 @@ class Editor {
             _font = new ar.graphics.Font;
         }
 
+        ///
         This setup(){
             string fontPath;
             version(OSX){
@@ -112,7 +113,7 @@ class Editor {
         This cursorPosition(in ar.math.Vector2i p){
             import std.algorithm:clamp;
             auto upperLimitY = p.y.clamp(0, _text.lines.length-1);
-            auto upperLimitX = p.x.clamp(0, _text.lines[upperLimitY].content.length-1);
+            auto upperLimitX = p.x.clamp(0, _text.lines[upperLimitY].content.length);
             _cursorPosition = ar.math.Vector2i(p.x.clamp(0, upperLimitX),
                                                p.y.clamp(0, upperLimitY));
             return this;
@@ -130,7 +131,7 @@ class Editor {
             assert(editor.cursorPosition == ar.math.Vector2i(1, 0));
 
             editor.cursorPosition(ar.math.Vector2i(3, 0));
-            assert(editor.cursorPosition == ar.math.Vector2i(2, 0));
+            assert(editor.cursorPosition == ar.math.Vector2i(3, 0));
 
             editor.cursorPosition(ar.math.Vector2i(1, 3));
             assert(editor.cursorPosition == ar.math.Vector2i(1, 1));
@@ -138,18 +139,95 @@ class Editor {
 
         ///
         This insert(in char c){
-            //TODO
-            _text.lines[_cursorPosition.y].content = _text.lines[_cursorPosition.y].content[0.._cursorPosition.x] ~ c ~ _text.lines[_cursorPosition.y].content[_cursorPosition.x .. $];
+            immutable head = _text.lines[_cursorPosition.y].content[0.._cursorPosition.x];
+            immutable tail = _text.lines[_cursorPosition.y].content[_cursorPosition.x..$];
+            switch (c) {
+                case '\n':
+                    auto newLine =  Line();
+                    newLine.lines = _cursorPosition.y+1;
+                    newLine.content = tail;
+                    _text.lines[_cursorPosition.y].content =  head;
+                    import std.array:insertInPlace;
+                    _text.lines.insertInPlace(_cursorPosition.y+1, newLine);
+                    cursorPosition = ar.math.Vector2i(0, cursorPosition.y+1);
+                    break;
+                default:
+                    _text.lines[_cursorPosition.y].content =  head ~ c ~ tail;
+                    cursorPosition = ar.math.Vector2i(cursorPosition.x+1, cursorPosition.y);
+            }
             return this;
         }
+
         unittest{
             auto editor = new Editor;
+
             editor.loadString("foo\nbar")
-                .cursorPosition(ar.math.Vector2i(1, 0))
-                .insert('x')
-                ;
+                  .cursorPosition(ar.math.Vector2i(1, 0))
+                  .insert('x');
             assert(editor._text.toString == "fxoo\nbar");
+            assert(editor.cursorPosition == ar.math.Vector2i(2, 0));
+
+            editor.cursorPosition(ar.math.Vector2i(3, 1))
+                  .insert('x');
+            assert(editor._text.toString == "fxoo\nbarx");
+
+            editor.cursorPosition(ar.math.Vector2i(2, 0))
+                  .insert('\n');
+            assert(editor._text.toString == "fx\noo\nbarx");
+            assert(editor._text.lines.length == 3);
+            assert(editor.cursorPosition == ar.math.Vector2i(0, 1));
+
+            editor.cursorPosition(ar.math.Vector2i(4, 2))
+                  .insert('\n');
+            assert(editor._text.toString == "fx\noo\nbarx\n");
+            assert(editor._text.lines.length == 4);
+            assert(editor.cursorPosition == ar.math.Vector2i(0, 3));
         }
+
+        ///
+        This remove(){
+            if (_cursorPosition.x == 0) {
+                if (_cursorPosition.y == 0) {
+                    // none
+                }else{
+                    immutable preLineLength =  _text.lines[_cursorPosition.y-1].content.length;
+
+                    auto newLine = Line();
+                    newLine.lines = _cursorPosition.y-1;
+                    newLine.content = _text.lines[_cursorPosition.y-1].content ~ _text.lines[_cursorPosition.y].content;
+                    _text.lines[_cursorPosition.y-1] = newLine;
+                    import std.algorithm:remove;
+                    _text.lines = _text.lines.remove(_cursorPosition.y);
+                    import std.conv:to;
+                    cursorPosition = ar.math.Vector2i(preLineLength.to!int, cursorPosition.y-1);
+                }
+            }else{
+                immutable head = _text.lines[_cursorPosition.y].content[0.._cursorPosition.x-1];
+                immutable tail = _text.lines[_cursorPosition.y].content[_cursorPosition.x..$];
+                _text.lines[_cursorPosition.y].content =  head ~ tail;
+                cursorPosition = ar.math.Vector2i(cursorPosition.x-1, cursorPosition.y);
+            }
+            return this;
+        }
+
+        unittest{
+            auto editor = new Editor;
+
+            editor.loadString("hoge\nmoge")
+                  .cursorPosition(ar.math.Vector2i(1, 0))
+                  .remove;
+            assert(editor._text.toString == "oge\nmoge");
+
+            editor.cursorPosition(ar.math.Vector2i(3, 0))
+                  .remove;
+            assert(editor._text.toString == "og\nmoge");
+
+            editor.cursorPosition(ar.math.Vector2i(0, 1))
+                  .remove;
+            assert(editor._text.toString == "ogmoge");
+            assert(editor.cursorPosition == ar.math.Vector2i(2, 0));
+        }
+
     }//public
 
     private{
